c00c6eb6c2b052e2e6edaf5ec7f5ef8c25155798
John Tucker <john@larkintuckerllc.com>
2016-09-29 14:14:14 -0400

    Incremental

--- src/index.js
+++ src/index.js
@@ -1,20 +1,27 @@
-// import io from 'socket.io-client';
+import io from 'socket.io-client';
 
-let base;
-let token;
-export const setBase = (b) => {
-  base = b;
+let baseHttp;
+let baseSocket;
+const channelStorage = parseInt(window.localStorage.getItem('thr0w_channel'), 10);
+let channel = isNaN(channelStorage) ? null : channelStorage;
+let token = window.localStorage.getItem('thr0w_token');
+let socket = null;
+export const setBaseHttp = (b) => {
+  baseHttp = b;
 };
-export const authenticated = () =>
-  window.localStorage.getItem('token') !== null;
+export const setBaseSocket = (b) => {
+  baseSocket = b;
+};
+export const getChannel = () => channel;
+export const authenticated = () => token !== null;
 export const login = (username, password) => {
-  if (base === undefined) throw new Error();
+  if (baseHttp === undefined) throw new Error();
   if (username === undefined || typeof username !== 'string') throw new Error();
   if (password === undefined || typeof password !== 'string') throw new Error();
   return new Promise((resolve, reject) => {
     const xmlhttp = new XMLHttpRequest();
     xmlhttp.open('POST',
-      `${base}/api/login/`, true);
+      `${baseHttp}/api/login/`, true);
     xmlhttp.setRequestHeader('Content-type',
       'application/x-www-form-urlencoded');
     xmlhttp.onreadystatechange = () => {
@@ -30,16 +37,49 @@ export const login = (username, password) => {
         reject({ message: '500' });
         return;
       }
-      if (!token) {
+      if (token === null) {
         reject({ message: '500' });
         return;
       }
-      window.localStorage.setItem('token', token);
+      window.localStorage.setItem('thr0w_token', token);
       resolve();
     };
     xmlhttp.send(`username=${username}&password=${password}`);
   });
 };
 export const logout = () => {
-  window.localStorage.removeItem('token');
+  window.localStorage.removeItem('thr0w_token');
+};
+export const connect = (c, msgCb) => {
+  if (baseSocket === undefined) throw new Error();
+  if (!authenticated()) throw new Error();
+  if (c === undefined || typeof c !== 'number') throw new Error();
+  if (msgCb === undefined || typeof msgCb !== 'function') throw new Error();
+  if (socket !== null) return Promise.resolve();
+  return new Promise((resolve, reject) => {
+    let connected = false;
+    const timeout = window.setTimeout(() => {
+      socket.disconnect();
+      socket = null;
+      reject({ message: '500' });
+    }, 5000);
+    const authenticate = () => {
+      socket.emit('authenticate',
+        JSON.stringify({ token, channel: c }));
+    };
+    socket = io(baseSocket);
+    socket.on('authenticated', () => {
+      if (connected) return;
+      connected = true;
+      channel = c;
+      window.localStorage.setItem('thr0w_channel', channel.toString());
+      window.clearTimeout(timeout);
+      socket.on('message', msgCb);
+      // TODO: HANDLE DUPLICATE
+      socket.on('duplicate', () => {});
+      resolve(channel);
+    });
+    socket.on('reconnect', authenticate);
+    authenticate();
+  });
 };
